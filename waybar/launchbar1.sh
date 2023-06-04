c8f98731421d7ff88d25871e0831bf97b1dcb8ea
JetpackJackson <firefoxyblock@gmail.com>
2023-06-04 16:05:54 -0400

    test

--- waybar/launchbar1.sh
+++ waybar/launchbar1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 killall waybar
 exec waybar -c ~/.config/waybar/config -s ~/.config/waybar/style.css
-#swaymsg output "*" bg /home/jet/Pictures/archbox.png fill
+output "*" bg /home/jet/Pictures/archbox.png fill
 killall swaybg
 exec swaybg -i /home/jet/Pictures/archbox.png -m fill
--- waybar/launchbar2.sh
+++ waybar/launchbar2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 killall waybar
 exec waybar -c ~/.config/waybar/aerzte -s ~/.config/waybar/aerzte.css
-#swaymsg output "*" bg /home/jet/Pictures/drum2.png fill
+output "*" bg /home/jet/Pictures/drum2.png fill
 killall swaybg
 exec swaybg -i /home/jet/Pictures/drum2.png -m fill
