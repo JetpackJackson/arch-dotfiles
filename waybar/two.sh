68ac781c3a07c571774cea92a7e260a4e8a0b637
JetpackJackson <firefoxyblock@gmail.com>
2023-06-11 14:44:12 -0400

    up

---
+++ waybar/two.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set $disp1 LVDS-1
+set $disp2 HDMI-A-1
+workspace 1 output $disp1
+workspace 2 output $disp1
+workspace 3 output $disp1
+workspace 4 output $disp1
+
+workspace 5 output $disp2
+workspace 6 output $disp2
+workspace 7 output $disp2
+workspace 8 output $disp2
+workspace 9 output $disp2
+workspace 10 output $disp2
