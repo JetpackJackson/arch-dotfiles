5af8e5a6cf54e9059d5d27217d7f2370fe1bf89d
JetpackJackson <baileyannew@tutanota.com>
2023-09-06 22:08:06 -0400

    add lua

---
+++ lazytest/lua/plugins/disabled.lua
@@ -0,0 +1,8 @@
+return {
+  { "folke/trouble.nvim", enabled = false },
+  { "L3MON4D3/LuaSnip", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
+  { "jose-elias-alvarez/null-ls.nvim", enabled = false },
+  { "hrsh7th/cmp-nvim-lsp", enabled = false },
+  { "lewis6991/gitsigns.nvim", enabled = false },
+}
---
+++ lazytest/lua/plugins/markdown.lua
@@ -0,0 +1,10 @@
+return {
+  { "0x00-ketsu/markdown-preview.nvim" }
+  -- Configure LazyVim to load gruvbox
+--  {
+--    "LazyVim/LazyVim",
+--    opts = {
+--      colorscheme = "gruvbox",
+--    },
+--  },
+}
