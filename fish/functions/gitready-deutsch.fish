08ca8b8157cc167c6d444c80269487fd0570865f
JetpackJackson <baileyannew@tutanota.com>
2023-09-24 08:33:55 -0400

    stuff

---
+++ fish/functions/gitready-deutsch.fish
@@ -0,0 +1,4 @@
+function gitready-deutsch --wraps='. ~/docs/scripts/gitready-deutsch' --description 'alias gitready-deutsch=. ~/docs/scripts/gitready-deutsch'
+  . ~/docs/scripts/gitready-deutsch $argv
+        
+end
