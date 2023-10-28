d7c81520eef770ad557cecd44bdc3f3521223e0a
JetpackJackson <baileyannew@tutanota.com>
2023-10-27 22:00:49 -0400

    update kitty

---
+++ scripts/opts.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+#typeset -a os
+#for o in `expac -S '%o\n' $pkgs`
+#do
+#  read -p "Install ${o}? " r
+#  [[ ${r,,} =~ ^y(|e|es)$ ]] && os+=( $o )
+#done
+#expac -S "%n %o" $pkg | pacman -S -
+#sudo pacman -S $pkgs ${os[@]}
