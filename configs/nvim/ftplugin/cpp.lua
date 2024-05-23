0fb2f2deb29f6bed6b7c71594f3bc12fc665d374
JetpackJackson <baileyannew@tutanota.com>
2024-05-23 07:56:05 -0400

    nvim: add cmd to compile all cpp files

--- configs/nvim/ftplugin/cpp.lua
+++ configs/nvim/ftplugin/cpp.lua
@@ -22,3 +22,23 @@ vim.keymap.set({'n'}, '<A-r>', '', {
     desc = "Run .cpp file via Neovim built-in terminal",
     callback = run_curr_cpp_file
 })
+
+local function run_all_cpp_files()
+    -- Get terminal codes for running cpp file
+    -- ("i" to enter insert before typing rest of the command)
+    local cpp_cmd = vim.api.nvim_replace_termcodes("ig++ *.cpp && ./a.out<cr>", true, false, true)
+    -- Determine terminal window split and launch terminal
+    local percent_of_win = 0.4
+    local curr_win_height = vim.api.nvim_win_get_height(0) -- Current window height
+    local term_height = math.floor(curr_win_height * percent_of_win) -- Terminal height
+    vim.cmd(":below " .. term_height .. "split | term") -- Launch terminal (horizontal split)
+
+    -- Press keys to run cpp command on current file
+    vim.api.nvim_feedkeys(cpp_cmd, "t", false)
+end
+
+vim.keymap.set({'n'}, '<A-t>', '', {
+    desc = "Run .cpp files via Neovim built-in terminal",
+    callback = run_all_cpp_files
+})
+
