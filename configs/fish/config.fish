c8c023cc8b71fce4ebbca6bca280f089f797f85a
JetpackJackson <baileyannew@tutanota.com>
2024-06-05 15:56:24 -0400

    fish: fix fetch further

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -147,10 +147,10 @@ if status is-interactive
     end
 
     function fetch --description 'fastfetch + cowsay'
-        fortune deutsch | cowsay -W 30 > cow
-        #set cowlength $(wc -l cow)
-        set cowlength $(wc -l cow | awk '{print $1}')
-        fastfetch --raw cow --logo-width 30 --logo-height $cowlength
+        fortune deutsch | cowsay -W 30 > /tmp/cow
+        set cowlength $(cat /tmp/cow | wc -l)
+        #set cowlength $(wc -l /tmp/cow | awk '{print $1}')
+        fastfetch --raw /tmp/cow --logo-width 30 --logo-height $cowlength
         #fortune deutsch | cowsay -W 30 | fastfetch --file -
     end
 
