5bb3279c510bd86320366d07db8601183826181e
JetpackJackson <baileyannew@tutanota.com>
2024-06-21 10:15:08 -0400

    fish fetch

--- configs/fish/functions/fetch.fish
+++ configs/fish/functions/fetch.fish
@@ -1,7 +1,7 @@
 function fetch --description 'fastfetch + cowsay'
-    fortune deutsch | cowsay -W 30 > /tmp/cow
+    fortune deutsch | cowsay -W 34 > /tmp/cow
     set cowlength $(cat /tmp/cow | wc -l)
     #set cowlength $(wc -l /tmp/cow | awk '{print $1}')
-    fastfetch --raw /tmp/cow --logo-width 30 --logo-height $cowlength
+    fastfetch --raw /tmp/cow --logo-width 34 --logo-height $cowlength
     #fortune deutsch | cowsay -W 30 | fastfetch --file -
 end
