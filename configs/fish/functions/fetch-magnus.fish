28663661716c94eeba723191e5297223dd0028fb
JetpackJackson <baileyannew@tutanota.com>
2024-07-06 11:21:50 -0400

    add img and fetch

---
+++ configs/fish/functions/fetch-magnus.fish
@@ -0,0 +1,4 @@
+function fetch-magnus --description 'fastfetch + magneto'
+    # sticker from: https://old.reddit.com/r/Xmen97/comments/1dgqhgf/all_stickers_design_complete_which_one_am_i/
+    fastfetch --logo /home/jet/.config/fish/images/mags-sticker-rm.png --logo-type kitty-direct --logo-width 35 --logo-height 15 --color red
+end
