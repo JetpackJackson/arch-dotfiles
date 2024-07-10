aa03d7051bb6dadd1f5968ceb72537015499c593
JetpackJackson <baileyannew@tutanota.com>
2024-07-10 15:51:31 -0400

    fetch guix fish

---
+++ configs/fish/functions/fetch-guix.fish
@@ -0,0 +1,3 @@
+function fetch-guix --description 'fastfetch + GNU'
+    fastfetch --logo /home/jet/.config/fish/images/guix.png --logo-type kitty-direct --logo-width 30 --logo-height 15 --color yellow
+end
