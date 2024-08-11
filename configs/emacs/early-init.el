ac04ab3fbd0099a5b6397875a0a94294af2f7d2f
JetpackJackson <jetpackjackson@katzenmue.de>
2024-08-11 16:14:11 -0400

    wl-copy bomb

--- configs/emacs/early-init.el
+++ configs/emacs/early-init.el
@@ -109,21 +109,29 @@
                 "corfu" "cape" "tempel"))
 
 ;; credit: yorickvP on Github
-(setq wl-copy-process nil)
-(defun wl-copy (text)
-  (setq wl-copy-process (make-process :name "wl-copy"
-                                      :buffer nil
-                                      :command '("wl-copy" "-f" "-n")
-                                      :connection-type 'pipe))
-                                      ;:noquery t))
-  (process-send-string wl-copy-process text)
-  (process-send-eof wl-copy-process))
-(defun wl-paste ()
-  (if (and wl-copy-process (process-live-p wl-copy-process))
-      nil ; should return nil if we're the current paste owner
-      (shell-command-to-string "wl-paste -n"))); | tr -d \r")))
-(setq interprogram-cut-function 'wl-copy)
-(setq interprogram-paste-function 'wl-paste)
+;(setq wl-copy-process nil)
+;(defun wl-copy (text)
+;  (setq wl-copy-process (make-process :name "wl-copy"
+;                                      :buffer nil
+;                                      :command '("wl-copy" "-f" "-n")
+;                                      :connection-type 'pipe))
+;                                      ;:noquery t))
+;  (process-send-string wl-copy-process text)
+;  (process-send-eof wl-copy-process))
+;(defun wl-paste ()
+;  (if (and wl-copy-process (process-live-p wl-copy-process))
+;      nil ; should return nil if we're the current paste owner
+;      (shell-command-to-string "wl-paste -n"))); | tr -d \r")))
+;(setq interprogram-cut-function 'wl-copy)
+;(setq interprogram-paste-function 'wl-paste)
+
+;(defun wl-copy (text)
+;  (let ((p (make-process :name "wl-copy"
+;                         :command '("wl-copy")
+;                         :connection-type 'pipe)))
+;    (process-send-string p text)
+;    (process-send-eof p)))
+;(setq interprogram-cut-function 'wl-copy)
 
 ;; https://stackoverflow.com/questions/4872088/is-there-any-way-for-subtasks-to-inherit-deadlines-in-org-mode
 (defun org-insert-sub-task ()
