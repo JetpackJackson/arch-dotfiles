d0dad8d7e2359e060c8f4d01d836565ef6d323e9
JetpackJackson <jetpackjackson@katzenmue.de>
2024-08-19 09:11:26 -0400

    org mode

--- configs/emacs/init.el
+++ configs/emacs/init.el
@@ -158,7 +158,10 @@
           interprogram-paste-function 'wl-paste-handler)))
 
 ;; org
-(use-package org :config
+(use-package org :ensure org-roam-ui
+  ;:ensure org-contrib
+  ;:config (require 'ox-extra) (ox-extras-activate '(ignore-headlines))
+  :config
   (setq org-agenda-span 20
 	org-agenda-start-on-weekday nil
 	org-agenda-start-day "-3d")
@@ -174,6 +177,24 @@
   (set-face-attribute 'org-level-3 nil :foreground "white"))
   ;(set-face-attribute 'org-special-keyword nil :foreground "lightslategray")
 (add-hook 'org-mode-hook 'org-indent-mode)
+
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename "~/docs/notes/notes-zettelkasten/"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol))
 
 ;; Follow the links
 ;(setq org-return-follows-link t)
