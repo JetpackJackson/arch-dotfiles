22660fc14470a8528f83fd8300994a73d31ab7d9
JetpackJackson <jetpackjackson@katzenmue.de>
2024-09-13 16:34:40 -0400

    line numbers

--- configs/emacs/lsp.el
+++ configs/emacs/lsp.el
@@ -95,7 +95,8 @@
 
 (add-hook 'compilation-mode-hook 'visual-line-mode)
 ;;(add-hook 'compilation-mode-hook 'display-line-numbers-mode 0) ;; FIXME find a cleaner way to disable
-(dolist (mode '(compilation-mode-hook))
+(dolist (mode '(compilation-mode-hook
+		eat-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 (add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
