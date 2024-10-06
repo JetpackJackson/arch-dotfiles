5d9cb7c9b4717da45ae7badaf3cd5d9ac7f9af92
JetpackJackson <jetpackjackson@katzenmue.de>
2024-10-06 16:14:51 -0400

    fix configs

--- shared/scripts/yams.sh
+++ shared/scripts/yams.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 killall yambar
-CONFIG=$HOME/.config/yambar/config-$(cat /etc/hostname).yml
+CONFIG=$HOME/.config/yambar/config.yml
 swaymsg -t get_outputs -r | jq -r '.[] | .name' | while read out ; do
   out_config=/tmp/yambar-config-$out.yml
   cat $CONFIG | sed "s/###monitor: .*$/monitor: $out/" > $out_config
