67c87a02b4585580731317891ec598621e784f2d
JetpackJackson <jetpackjackson@katzenmue.de>
2024-10-08 13:52:27 -0400

    idle

---
+++ shared/scripts/dwlidle.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+HOSTNAME=$(cat /etc/hostname)
+if [[ $HOSTNAME == "weasel" ]]; then
+swayidle -w \
+    timeout 300 'swaylock -f --clock --datestr '%d.%m.%Y' --indicator -ef -i "/home/jet/pics/wallpapers/lock.jpg"' \
+    timeout 600 'wlr-randr eDP-1 --off' resume 'wlr-randr eDP-1 --on' \
+    before-sleep 'swaylock -f --clock --datestr '%d.%m.%Y' --indicator -ef -i "/home/jet/pics/wallpapers/lock.jpg"'
+fi
