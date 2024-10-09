5f25877621df8b3d70cb50b55d69184a87573c9f
JetpackJackson <jetpackjackson@katzenmue.de>
2024-10-08 21:15:59 -0400

    fish

--- shared/scripts/dwlidle.sh
+++ shared/scripts/dwlidle.sh
@@ -1,8 +1,12 @@
-#!/usr/bin/env bash
-HOSTNAME=$(cat /etc/hostname)
-if [[ $HOSTNAME == "weasel" ]]; then
-swayidle -w \
-    timeout 300 'swaylock -f --clock --datestr '%d.%m.%Y' --indicator -ef -i "/home/jet/pics/wallpapers/lock.jpg"' \
-    timeout 600 'wlr-randr --output eDP-1 --off' resume 'wlr-randr --output eDP-1 --on' \
-    before-sleep 'swaylock -f --clock --datestr '%d.%m.%Y' --indicator -ef -i "/home/jet/pics/wallpapers/lock.jpg"'
-fi
+#!/usr/bin/env fish
+#HOSTNAME=$(cat /etc/hostname)
+#if [[ $HOSTNAME == "weasel" ]]; then
+
+set HOSTNAME $(cat /etc/hostname)
+if test $HOSTNAME = "weasel"
+    swayidle -w \
+        timeout 300 'swaylock -f --clock --datestr '%d.%m.%Y' --indicator -ef -i "/home/jet/pics/wallpapers/lock.jpg"' \
+        timeout 600 'killall yambar && wlr-randr --output eDP-1 --off' resume 'wlr-randr --output eDP-1 --on && notify-send "welcome back!" && fish -c "killall yambar; yambar -c ~/.config/yambar/config-dwl.yml &; disown"' \
+        before-sleep 'swaylock -f --clock --datestr '%d.%m.%Y' --indicator -ef -i "/home/jet/pics/wallpapers/lock.jpg"' &; disown
+end
+#fi
