771ac46406a6c4a87c2339e1f48ff3fed8b94734
JetpackJackson <jetpackjackson@katzenmue.de>
2024-11-09 14:21:46 -0500

    music and borg

--- shared/scripts/copy-to-droid.sh
+++ shared/scripts/copy-to-droid.sh
@@ -1,39 +1,38 @@
 #!/usr/bin/env bash
 
-beet up
-beet splupdate
+pipbeet/bin/beet up
+pipbeet/bin/beet splupdate
 cp ~/music/playlists/everything.m3u ~/music-android/playlists/everything.m3u
 cp ~/music/playlists/deutschemusik.m3u ~/music-android/playlists/deutschemusik.m3u
 sed -i -e 's/^/\/storage\/emulated\/0\/Music\//' ~/music-android/playlists/deutschemusik.m3u
 sed -i -e 's/^/\/storage\/emulated\/0\/Music\//' ~/music-android/playlists/everything.m3u
 
-beet convert -y 'artist::^Cro$'
-beet convert -y "artist::^Lotte$"
-beet convert -y 'artist::^LOTTE$'
-beet convert -y 'artist::^Love A$'
-beet convert -y 'artist:^Marv$'
-beet convert -y 'artist:Phil Collins' 'fremde'
-beet convert -y 'artist:David Bowie' 'helden'
+# beet convert -y 'artist::^Cro$'
+# beet convert -y "artist::^Lotte$"
+# beet convert -y 'artist::^LOTTE$'
+# beet convert -y 'artist::^Love A$'
+# beet convert -y 'artist:^Marv$'
+# beet convert -y 'artist:Phil Collins' 'fremde'
+# beet convert -y 'artist:David Bowie' 'helden'
 
-#rsync -aAXHvu $1 --delete --relative --files-from=/home/jet/music/playlists/deutschemusik.m3u /home/jet/music/./ ~/music-android/
 awk -F '/' '{ print $2 }' < ~/music/playlists/deutschemusik.m3u | sort | uniq > /tmp/deutsch.txt
-sed -i '/LOTTE/d' /tmp/deutsch.txt
-sed -i '/Cro/d' /tmp/deutsch.txt
-sed -i '/Love A/d' /tmp/deutsch.txt
-sed -i '/Marv/d' /tmp/deutsch.txt
-sed -i '/Phil Collins/d' /tmp/deutsch.txt
-sed -i '/David Bowie/d' /tmp/deutsch.txt
-while read -r artist; do
-    echo "converting $artist"
-    beet convert -y "artist:$artist"
-    beet convert -y "albumartist:$artist"
-done < /tmp/deutsch.txt
-#rsync -aAXHvu $1 --delete --relative --files-from=/home/jet/music/playlists/aufenglisch.m3u /home/jet/music/./ ~/music-android/
-awk -F '/' '{ print $2 }' < ~/music/playlists/aufenglisch.m3u | sort | uniq > /tmp/auf.txt
-while read -r artist; do
-    beet convert -y "artist:$artist"
-done < /tmp/auf.txt
+# sed -i '/LOTTE/d' /tmp/deutsch.txt
+# sed -i '/Cro/d' /tmp/deutsch.txt
+# sed -i '/Love A/d' /tmp/deutsch.txt
+# sed -i '/Marv/d' /tmp/deutsch.txt
+# sed -i '/Phil Collins/d' /tmp/deutsch.txt
+# sed -i '/David Bowie/d' /tmp/deutsch.txt
+# while read -r artist; do
+#     echo "converting $artist"
+#     beet convert -y "artist:$artist"
+#     beet convert -y "albumartist:$artist"
+# done < /tmp/deutsch.txt
+# awk -F '/' '{ print $2 }' < ~/music/playlists/aufenglisch.m3u | sort | uniq > /tmp/auf.txt
+# while read -r artist; do
+#     beet convert -y "artist:$artist"
+# done < /tmp/auf.txt
 
+pipbeet/bin/beet alt update droid
 
 # lrc files
 find ~/music/ -name "*.lrc" -printf '%P\n' > /tmp/lrc-all.txt
@@ -46,5 +45,3 @@ while read -r file; do
     fi
 done < ~/music/playlists/deutschemusik.m3u
 rsync -aAXHvu $1 --delete --relative --files-from=/tmp/lrc.txt /home/jet/music/./ ~/music-android/
-
-
